6ce6bd69a923981c9112d72f0ebe2896759d7aa7
Sara Derakhshani <saraderakhshani@icloud.com>
2020-07-29 14:56:06 +0200

    Test path existence

--- classifier.py
+++ classifier.py
@@ -6,6 +6,11 @@
 
 from process_data import HeadlineData
 import csv
+import os
+import logging
+
+logging.basicConfig(filename='classifier.log', level=logging.INFO,
+                    format='%(asctime)s %(message)s')
 
 
 class Classifier:
@@ -16,32 +21,46 @@ class Classifier:
         self.headlines = HeadlineData(datafile)
 
     def train_model(self):
-        self.compute_single_statistics()
-        sarcasm_count = 0
-        nonsarcasm_count = 0
-        sarcastic_stats = {'average_word_length': 0}
-        nonsarcastic_stats = {'average_word_length': 0}
-        with open(self.single_stats) as csv_file:
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
-                if int(row[2]) == 0:
-                    nonsarcasm_count += 1
-                    nonsarcastic_stats['average_word_length'] += float(row[3])
-                else:
-                    sarcasm_count += 1
-                    sarcastic_stats['average_word_length'] += float(row[3])
-        awl_sarcastic = (sarcastic_stats['average_word_length'] / sarcasm_count)
-        awl_nonsarcastic = (nonsarcastic_stats['average_word_length'] / nonsarcasm_count)
-        self.add_csv_entry(self.class_stats, [0, awl_nonsarcastic])
-        self.add_csv_entry(self.class_stats, [1, awl_sarcastic])
+        """Create classification model as csv file containing statistics
+        for each class
+        Compute class statistics from single statistics in csv file format
+        """
+        # Test if model has been trained already
+        if os.path.exists(self.single_stats):
+            logging.info('Model is trained and ready to predict')
+        # Sum up the figures from each feature in the single statistics for
+        # the sarcastic and nonsarcastic class and divide by class count to
+        # compute the mean figure for each feature
+        # Create csv file to save class statistics
+        else:
+            self._compute_single_statistics()
+            sarcasm_count = 0
+            nonsarcasm_count = 0
+            sarcastic_stats = {'average_word_length': 0}
+            nonsarcastic_stats = {'average_word_length': 0}
+            with open(self.single_stats) as csv_file:
+                csv_reader = csv.reader(csv_file)
+                for row in csv_reader:
+                    if int(row[2]) == 0:
+                        nonsarcasm_count += 1
+                        nonsarcastic_stats['average_word_length'] += float(row[3])
+                    else:
+                        sarcasm_count += 1
+                        sarcastic_stats['average_word_length'] += float(row[3])
+            awl_sarcastic = (sarcastic_stats['average_word_length'] / sarcasm_count)
+            awl_nonsarcastic = (nonsarcastic_stats['average_word_length'] / nonsarcasm_count)
+            self.add_csv_entry(self.class_stats, [0, awl_nonsarcastic])
+            self.add_csv_entry(self.class_stats, [1, awl_sarcastic])
 
-    def compute_single_statistics(self):
-        for avl in self.headlines.average_word_lengths():
-            self.add_csv_entry(self.single_stats,
-                               [avl[0].id,
-                                avl[0].headline,
-                                avl[0].sarcasm,
-                                avl[1]])
+    def _compute_single_statistics(self):
+        for feature in self.headlines.features:
+            for figure in feature:
+                self.add_csv_entry(self.single_stats,
+                                   [figure[0].id,
+                                    figure[0].headline,
+                                    figure[0].sarcasm,
+                                    figure[1]])
+        logging.info('Single statistics have been computed')
 
     def add_csv_entry(self, csv_file, new_entry):
         """Add entry to csv file"""
