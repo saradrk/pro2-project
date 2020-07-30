44b916bdbdf7abae742af849191bd7b63c60226e
Sara Derakhshani <saraderakhshani@icloud.com>
2020-07-30 13:47:28 +0200

    Compute features in data class for better further use

--- process_data.py
+++ process_data.py
@@ -7,6 +7,7 @@ import logging
 import json
 from headline import Headline
 import spacy
+import csv
 
 logging.basicConfig(filename='process_data.log', level=logging.INFO,
                     format='%(asctime)s %(message)s')
@@ -14,12 +15,13 @@ logging.basicConfig(filename='process_data.log', level=logging.INFO,
 
 class HeadlineData:
 
-    def __init__(self, data_file, language_model='en_core_web_sm'):
+    def __init__(self, data_file, out_csv, language_model='en_core_web_sm'):
         self.data = []
         self.model = language_model
         self.nlp = spacy.load(language_model)
         self._process_file(data_file)
-        self.features = self.generate_features()
+        self.features = self._generate_features()
+        self.out_csv = out_csv 
 
     def _process_file(self, filename):
         """Read data from file
@@ -49,7 +51,7 @@ class HeadlineData:
         doc = self.nlp(json_entry['headline'])
         return Headline(id_number, json_entry, doc)
 
-    def generate_features(self):
+    def _generate_features(self):
         n = 0
         while n < len(self.data):
             awl = self._awl(self.data[n])
@@ -70,6 +72,14 @@ class HeadlineData:
         stop_words = [1 for token in headline.doc if token.is_stop is True]
         aswc = sum(stop_words) / len(headline.tokens)
         return round(aswc, 2)
+
+    def compute_single_statistics(self):
+        with open(self.out_csv, mode='a+') as out_csv:
+            writer = csv.writer(out_csv)
+            for feature_list in self.features:
+                writer.writerow(feature_list)
+        logging.info('Single statistics computed')
+
 
     # def average_word_lengths(self):
     #     """Generator to get average word length"""
